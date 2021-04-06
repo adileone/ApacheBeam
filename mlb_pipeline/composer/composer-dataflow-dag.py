071ddc1689fb7eb36de15c311cea49d658a3748d
Alessandro Di Leone <a.dileone@goreply.io>
2021-04-06 17:46:34 +0200

    read pub sub todo decode

--- mlb_pipeline/composer/composer-dataflow-dag.py
+++ mlb_pipeline/composer/composer-dataflow-dag.py
@@ -1,14 +1,22 @@
 import datetime
+from base64 import b64encode
 
 from airflow import models
 from airflow.contrib.operators.dataflow_operator import DataflowTemplateOperator
 from airflow.utils.dates import days_ago
+from airflow.operators.bash_operator import BashOperator
+from airflow.contrib.sensors.pubsub_sensor import PubSubPullSensor
 
 bucket_path = models.Variable.get("bucket_path")
 project_id = models.Variable.get("project_id")
 gce_zone = models.Variable.get("gce_zone")
 gce_region = models.Variable.get("gce_region")
 
+echo_template = '''
+{% for m in task_instance.xcom_pull(task_ids='pull-messages') %}
+    echo "AckID: {{ m.get('ackId') }}, Base64-Encoded: {{ m.get('message') }}"
+{% endfor %}
+'''
 
 default_args = {
     # Tell airflow to start one day ago, so that it runs as soon as you upload it
@@ -29,27 +37,35 @@ default_args = {
 # DAG object.
 with models.DAG(
     # The id you will see in the DAG airflow page
-    "composer_dataflow_dag",
+    "new_dag_20sec",
     default_args=default_args,
     # The interval with which to schedule the DAG
     schedule_interval=datetime.timedelta(days=1),  # Override to match your needs
 ) as dag:
 
-    start_template_job = DataflowTemplateOperator(
-        # The task id of your job
-        task_id="dataflow_operator_run_pipeline_asap",
-        # The name of the template that you're using.
-        # Below is a list of all the templates you can use.
-        # For versions in non-production environments, use the subfolder 'latest'
-        # https://cloud.google.com/dataflow/docs/guides/templates/provided-batch#gcstexttobigquery
-        template="gs://beambinaries/templates/customTemplate",
-        # Use the link above to specify the correct parameters for your template.
-        # parameters={
-        #     "javascriptTextTransformFunctionName": "transformCSVtoJSON",
-        #     "JSONPath": bucket_path + "/jsonSchema.json",
-        #     "javascriptTextTransformGcsPath": bucket_path + "/transformCSVtoJSON.js",
-        #     "inputFilePattern": bucket_path + "/inputFile.txt",
-        #     "outputTable": project_id + ":average_weather.average_weather",
-        #     "bigQueryLoadingTemporaryDirectory": bucket_path + "/tmp/",
-        # },
-    )
+    subscription = 'test-sub'  # Cloud Pub/Sub subscription
+
+    t4 = PubSubPullSensor(task_id='pull-messages', ack_messages=True, project=project_id, subscription=subscription)
+    t5 = BashOperator(task_id='echo-pulled-messages',
+                      bash_command=echo_template)
+
+    # start_template_job = DataflowTemplateOperator(
+    #     # The task id of your job
+    #     task_id="dataflow_operator_run_pipeline_asap",
+    #     # The name of the template that you're using.
+    #     # Below is a list of all the templates you can use.
+    #     # For versions in non-production environments, use the subfolder 'latest'
+    #     # https://cloud.google.com/dataflow/docs/guides/templates/provided-batch#gcstexttobigquery
+    #     template="gs://beambinaries/templates/customTemplate",
+    #     # Use the link above to specify the correct parameters for your template.
+    #     # parameters={
+    #     #     "javascriptTextTransformFunctionName": "transformCSVtoJSON",
+    #     #     "JSONPath": bucket_path + "/jsonSchema.json",
+    #     #     "javascriptTextTransformGcsPath": bucket_path + "/transformCSVtoJSON.js",
+    #     #     "inputFilePattern": bucket_path + "/inputFile.txt",
+    #     #     "outputTable": project_id + ":average_weather.average_weather",
+    #     #     "bigQueryLoadingTemporaryDirectory": bucket_path + "/tmp/",
+    #     # },
+    # )
+
+    (t4 >> t5) #>> start_template_job
