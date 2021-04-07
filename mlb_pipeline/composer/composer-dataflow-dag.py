fccf493756d45781898b0955bf1cd0888451977a
Alessandro Di Leone <a.dileone@goreply.io>
2021-04-07 14:11:55 +0200

    update

--- mlb_pipeline/composer/composer-dataflow-dag.py
+++ mlb_pipeline/composer/composer-dataflow-dag.py
@@ -1,7 +1,5 @@
 import datetime
-import logging
-
-from base64 import b64encode
+# import json
 
 from airflow import models
 from airflow.contrib.operators.dataflow_operator import DataflowTemplateOperator
@@ -16,11 +14,11 @@ project_id = models.Variable.get("project_id")
 gce_zone = models.Variable.get("gce_zone")
 gce_region = models.Variable.get("gce_region")
 
-echo_template = '''
-{% for m in task_instance.xcom_pull(task_ids='pull-messages') %}
-    echo "AckID: {{ m.get('ackId') }}, Base64-Encoded: {{ m.get('message') }}"
-{% endfor %}
-'''
+# echo_template = '''
+# {% for m in task_instance.xcom_pull(task_ids='pull-messages') %}
+#     echo "AckID: {{ m.get('ackId') }}, Base64-Encoded: {{ m.get('message') }}"
+# {% endfor %}
+# '''
 
 default_args = {
     # Tell airflow to start one day ago, so that it runs as soon as you upload it
@@ -49,15 +47,20 @@ with models.DAG(
 
     subscription = 'test-sub'  # Cloud Pub/Sub subscription
 
-    t4 = PubSubPullSensor(task_id='pull-messages', ack_messages=True, project=project_id, subscription=subscription)
+    t4 = PubSubPullSensor(task_id='pull-messages', ack_messages=True, project=project_id, subscription=subscription, max_messages=1500)
 
     def print_context(**kwargs):
 
-        encoded = kwargs['ti'].xcom_pull(key="return_value")
-        logging.info(encoded)
+        encoded = kwargs['ti'].xcom_pull(task_ids='pull-messages', key="return_value")
+        # json_encoded = json.loads(encoded)
+        i=0
+        for m in encoded :
+            value = m["message"]["attributes"]["objectId"]
+            kwargs['ti'].xcom_push(key="message_"+str(i), value=value)
+            i=i+1
         
-        kwargs['ti'].xcom_push(key="message", value="message")
-        return "message pushed to Xcom"
+        # kwargs['ti'].xcom_push(key="message", value="message")
+        return "message"
 
 
     t5 = PythonOperator(
@@ -89,4 +92,4 @@ with models.DAG(
     #     # },
     # )
 
-    (t4)# >> t5) #>> start_template_job
+    (t4 >> t5) #>> start_template_job
